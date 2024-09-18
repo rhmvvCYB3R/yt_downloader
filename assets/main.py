dd5aeca768b627ee0f7088e8c5833f90771affcf
RHMVCYB3R <10ragimov10@gmail.com>
2024-09-18 07:30:49 +0400

    a little changes

---
+++ assets/main.py
@@ -0,0 +1,9 @@
+import customtkinter
+
+app = customtkinter.CTk()
+app.title("YT DOWNLOADER")
+app.iconbitmap("public/images/app.ico")
+app.geometry("720x480")
+app.resizable(False,False)
+
+app.mainloop()
