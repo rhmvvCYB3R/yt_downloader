af0c33e4cd517a700ba963983cc46df59d96d53d
RHMVCYB3R <10ragimov10@gmail.com>
2024-09-18 12:30:00 +0400

    some changes

--- assets/main.py
+++ assets/main.py
@@ -1,9 +1,21 @@
 import customtkinter
+import pytube
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+
 
 app = customtkinter.CTk()
 app.title("YT DOWNLOADER")
 app.iconbitmap("public/images/app.ico")
 app.geometry("720x480")
 app.resizable(False,False)
+
+link_entry = customtkinter.CTkEntry(app,width=400,height=35)
+link_entry.place(x=80,y=50)
+
+search_btn = customtkinter.CTkButton(app,text="Искать")
+search_btn.place(x=510,y=53)
+
 
 app.mainloop()
