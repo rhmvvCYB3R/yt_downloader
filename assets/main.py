994d8fd8489ca4ee153af4b00bc1b8621fa0f856
RHMVCYB3R <10ragimov10@gmail.com>
2024-09-18 13:54:17 +0400

    Добавлено Entry, Text_label, Button

--- assets/main.py
+++ assets/main.py
@@ -11,11 +11,22 @@ app.iconbitmap("public/images/app.ico")
 app.geometry("720x480")
 app.resizable(False,False)
 
-link_entry = customtkinter.CTkEntry(app,width=400,height=35)
-link_entry.place(x=80,y=50)
+text_label = customtkinter.CTkLabel(app,text="Youtube Video Downloader",font=("Compact",35),text_color="red")
+text_label.place(x=150,y=20)
+
+def clear_link_entry(event):
+    if link_entry.get() == "Вставьте ссылку на видео! ":
+        link_entry.delete(0, "end")
+     
+
+link_entry = customtkinter.CTkEntry(app,width=400,height=35,font=("UBUNTU" , 12),text_color="blue")
+link_entry.insert(0,"Вставьте ссылку на видео! ")
+link_entry.place(x=80,y=110)
+link_entry.bind("<FocusIn>",clear_link_entry)
+
 
 search_btn = customtkinter.CTkButton(app,text="Искать")
-search_btn.place(x=510,y=53)
+search_btn.place(x=510,y=113)
 
 
 app.mainloop()
